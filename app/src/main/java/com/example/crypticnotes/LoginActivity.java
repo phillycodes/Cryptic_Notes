5dde63975bb532ad2d57d40f4f0beaab490a2ac3
Philben Pierre <82547190+phillycodes@users.noreply.github.com>
2023-02-10 13:51:07 -0500

    Login Screen Is Functional with users already entered into the database. Sign-out feature is forthcoming. Updates made to activity_login.xml, LoginActivity.java, SplashActivity.java, created Utility.java class.

--- app/src/main/java/com/example/crypticnotes/LoginActivity.java
+++ app/src/main/java/com/example/crypticnotes/LoginActivity.java
@@ -1,14 +1,97 @@
 package com.example.crypticnotes;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText email_EditText, password_EditText;
+    Button login_Btn;
+    ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email_EditText = findViewById(R.id.email_editText);
+        password_EditText = findViewById(R.id.password_editText);
+        login_Btn = findViewById(R.id.loginBtn);
+        progressBar = findViewById(R.id.progress_bar);
+
+        login_Btn.setOnClickListener((v) -> loginUser());
+
     }
+    void loginUser(){
+        String email = email_EditText.getText().toString();
+        String password = password_EditText.getText().toString();
+
+        boolean isValidated = validateData(email, password);
+        if(!isValidated){
+            return;
+        }
+
+        loginAccountInFirebase(email, password);
+    }
+
+    void loginAccountInFirebase(String email, String password){
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        changeInProgress(true);
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                changeInProgress(false);
+                if (task.isSuccessful()) {
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    finish();
+                    // login is the user entered the correct credentials
+//                    if(firebaseAuth.getCurrentUser()){
+//                        // go to main activity
+//                    } else {
+//                        //
+//                    }
+                } else {
+                    // login failed
+                    Utility.showToast(LoginActivity.this, task.getException().getLocalizedMessage());
+                }
+            }
+        });
+    }
+
+    void changeInProgress(boolean inProgress){
+        if(inProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            login_Btn.setVisibility(View.GONE);
+        } else {
+            progressBar.setVisibility(View.GONE);
+            login_Btn.setVisibility(View.VISIBLE);
+        }
+    }
+
+    boolean validateData(String email, String password){
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            email_EditText.setError("Email is invalid!");
+            return false;
+        }
+        if (password.length() < 6 ){
+            password_EditText.setError("Minimum Characters requirement not met: 6 Character");
+            return false;
+        }
+        return true;
+    }
+
 }
--- app/src/main/java/com/example/crypticnotes/SplashActivity.java
+++ app/src/main/java/com/example/crypticnotes/SplashActivity.java
@@ -1,10 +1,13 @@
 package com.example.crypticnotes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -16,7 +19,13 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                if(currentUser == null){
+                    startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                } else {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                }
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
                 finish();
             }
         }, 1000);

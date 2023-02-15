2fc17f8077e6a78a4b8d001d8d176bb71b702db6
Philben Pierre <82547190+phillycodes@users.noreply.github.com>
2023-02-15 17:46:33 -0500

    Notes Screen Activity Created and a template of the notes screen is made. Added Cloud FireStore dependency to the gradle. Added "New Note" button and "save note" button.

---
+++ app/src/main/java/com/example/crypticnotes/NoteDetailsActivity.java
@@ -0,0 +1,37 @@
+package com.example.crypticnotes;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NoteDetailsActivity extends AppCompatActivity {
+
+    EditText titleEditText, contentEditText;
+    ImageButton saveNoteBtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_details);
+
+        titleEditText = findViewById(R.id.notes_title_text);
+        contentEditText = findViewById(R.id.notes_content_text);
+        saveNoteBtn = findViewById(R.id.save_note_btn);
+
+        saveNoteBtn.setOnClickListener((v) -> saveNote());
+
+    }
+
+    void saveNote() {
+        String noteTitle = titleEditText.getText().toString();
+        String noteContent = contentEditText.getText().toString();
+        if(noteTitle == null || noteTitle.isEmpty()) {
+            titleEditText.setError("Title is required");
+            return;
+        }
+    }
+
+}

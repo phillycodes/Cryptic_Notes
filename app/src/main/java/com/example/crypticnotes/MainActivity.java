58545559771da72ba6861303ea359fbba96d8d0b
Philben Pierre <82547190+phillycodes@users.noreply.github.com>
2023-02-16 13:35:53 -0500

    recycler_note_item.xml provides the template for the notes organization. NoteAdapter.java organizes the data onto the template. MainActivity.java sets up the recycler view by querying the data and organizing it by timestamp. Utilizing the onStart, onStop, and onResume methods we are able to update the NoteAdapter class which provides the recycler view with the updated content.

--- app/src/main/java/com/example/crypticnotes/MainActivity.java
+++ app/src/main/java/com/example/crypticnotes/MainActivity.java
@@ -2,14 +2,23 @@ package com.example.crypticnotes;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.Query;
 
 public class MainActivity extends AppCompatActivity {
 
     FloatingActionButton addNoteBtn;
+    RecyclerView recyclerView;
+    ImageButton menuBtn;
+    NoteAdapter noteAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +26,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         addNoteBtn = findViewById(R.id.add_note_btn);
+        recyclerView = findViewById(R.id.recycler_view);
+        menuBtn = findViewById(R.id.menu_btn);
 
         addNoteBtn.setOnClickListener((v) -> startActivity(new Intent(MainActivity.this, NoteDetailsActivity.class)));
+        menuBtn.setOnClickListener((v) -> showMenu() );
+        setupRecyclerView();
+    }
+
+    void showMenu() {
+        //TODO Display menu
+    }
+
+    void setupRecyclerView() {
+        Query query = Utility.getCollectionReferenceForNotes().orderBy("timestamp", Query.Direction.DESCENDING);
+        FirestoreRecyclerOptions<Note> options = new FirestoreRecyclerOptions.Builder<Note>()
+                .setQuery(query, Note.class).build();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        noteAdapter = new NoteAdapter(options, this);
+        recyclerView.setAdapter(noteAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        noteAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        noteAdapter.stopListening();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        noteAdapter.notifyDataSetChanged();
     }
 }
